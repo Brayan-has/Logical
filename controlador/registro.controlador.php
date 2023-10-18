86d8e937d7cecddce980d4ec138fe3d300984c49
Brayan-has <g.nayarb112@gmail.com>
2023-10-18 16:13:00 -0600

    feat: Registro-encriptación de contraseñas

--- controlador/registro.controlador.php
+++ controlador/registro.controlador.php
@@ -15,7 +15,7 @@ if(!empty($_POST["registro"])){
         $cedula = $_POST['cedula'];
         $telefono = $_POST['telefono'];
         $correo = $_POST['correo'];
-        $contrasena = $_POST['contrasena'];
+        $contrasena = md5($_POST['contrasena']);
 
       //sentencia para registrar el usuario
       $sql = $conexion->query("INSERT INTO empleado (nombre,apellido,fecha_nacimiento,cedula,telefono,correo,contrasena) VALUES ( '$nombre','$apellido','$nacimiento',$cedula,$telefono,'$correo','$contrasena')");
